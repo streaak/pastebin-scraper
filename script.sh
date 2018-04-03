86f32c2e1e973daf40e0d408bfef2716f6e6cb4a
streaak <streaksterz@gmail.com>
2018-04-03 21:19:27 +0530

    Change function name

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ] || [ $1 == '-h' ]; then
   exit 0;
 fi
 
-function test() {
+function scrape() {
         echo "Searching pastebin..."
         curl -s -X GET "$url" | python -m json.tool > ./output/$dir/output.json 
         cat ./output/$dir/output.json | jq -r '.data[] | .id' | awk '{print "https://psbdmp.ws/" $1 }' > ./output/$dir/urls.txt
@@ -19,7 +19,7 @@ if [[ $1 == '-g' ]] && [[ $2 != '' ]] ; then
 dir=general/$2
 url=https://psbdmp.ws/api/search/$2
 mkdir ./output/$dir/
-test
+scrape
 elif [[ $2 == '' ]]; then
         echo "Missing Value. Try to run ./script.sh -g <Term here>"
         exit 0
@@ -29,7 +29,7 @@ if [[ $1 == '-e' ]] && [[ $2 != '' ]] ; then
 dir=email/$2
 url=https://psbdmp.ws/api/search/email/$2
 mkdir ./output/$dir/
-test
+scrape
 elif [[ $2 == '' ]]; then
         echo "Missing Value. Try to run ./script.sh -e <Term here>"
         exit 0
@@ -39,7 +39,7 @@ if [[ $1 == '-d' ]] && [[ $2 != '' ]] ; then
 dir=domain/$2
 url=https://psbdmp.ws/api/search/domain/$2
 mkdir ./output/$dir/
-test
+scrape
 elif [[ $2 == '' ]]; then
         echo "Missing Value. Try to run ./script.sh -d <Term here>"
         exit 0
