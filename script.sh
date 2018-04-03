24d8bbe69da7e5994d72ec614f1a343c1de1ed1f
streaak <streaksterz@gmail.com>
2018-04-03 20:49:42 +0530

    Working script

--- script.sh
+++ script.sh
@@ -1,48 +1,49 @@
-#!/bin/bash
-
 if [ $# -eq 0 ] || [ $1 == '-h' ]; then
   echo "Usage:";
-  echo "Search for domains - ./script.sh -d domain.com" 
+  echo "Search for domains - ./script.sh -d domain.com"
   echo "Search for emails  - ./script.sh -e foo@bar.com"
   echo "General search     - ./script.sh -g foobar"
   exit 0;
 fi
 
-if [[ $1 == '-g' ]] && [[ $2 != '' ]] ; then
-        mkdir ./output/general/$2/
+dir=''
+url=''
+
+function test() {
         echo "Searching pastebin..."
-        curl -s -X GET "https://psbdmp.ws/api/search/$2" | python -m json.tool > ./output/general/$2/output.json #Get search data
-        cat ./output/general/$2/output.json | jq -r '.data[] | .id' | awk '{print "https://psbdmp.ws/" $1 }' > ./output/general/$2/urls.txt # Create urls.txt
-        echo "Task completed. Output present in ./output/general/$2/"
+        curl -s -X GET "$url" | python -m json.tool > ./output/$dir/output.json 
+        cat ./output/$dir/output.json | jq -r '.data[] | .id' | awk '{print "https://psbdmp.ws/" $1 }' > ./output/$dir/urls.txt
+        echo "output present in ./output/$dir/$2"
         exit 0
-        elif [[ $2 == '' ]]; then 
-        echo "Missing Value. Try to run ./script.sh -g <Term here>"
+        return 0
+}
+
+if [[ $1 == '-g' ]] && [[ $2 != '' ]] ; then
+dir=general/$2
+url=https://psbdmp.ws/api/search/$2
+mkdir ./output/$dir/
+test
+elif [[ $2 == '' ]]; then
+        echo "No input"
         exit 0
 fi
 
 if [[ $1 == '-e' ]] && [[ $2 != '' ]] ; then
-        mkdir ./output/email/$2/
-        echo "Searching pastebin..."
-        curl -s -X GET "https://psbdmp.ws/api/search/email/$2" | python -m json.tool > ./output/email/$2/output.json #Get search data
-        cat ./output/email/$2/output.json | jq -r '.data[] | .id' | awk '{print "https://psbdmp.ws/" $1 }' > ./output/email/$2/urls.txt # Create urls.txt
-        echo "Task completed. Output present in ./output/email/$2/"
-        exit 0
-        elif [[ $2 == '' ]]; then 
-        echo "Missing Value. Try to run ./script.sh -e <email here>"
+dir=email/$2
+url=https://psbdmp.ws/api/search/email/$2
+mkdir ./output/$dir/
+test
+elif [[ $2 == '' ]]; then
+        echo "No input"
         exit 0
 fi
 
 if [[ $1 == '-d' ]] && [[ $2 != '' ]] ; then
-        mkdir ./output/domain/$2/
-        echo "Searching pastebin..."
-        curl -s -X GET "https://psbdmp.ws/api/search/domain/$2" | python -m json.tool > ./output/domain/$2/output.json #Get search data
-        cat ./output/domain/$2/output.json | jq -r '.data[] | .id' | awk '{print "https://psbdmp.ws/" $1 }' > ./output/domain/$2/urls.txt # Create urls.txt
-        echo "Task completed. Output present in ./output/domain/$2/"
-        exit 0
-        elif [[ $2 == '' ]]; then
-        echo "Missing Value. Try to run ./script.sh -d <domain here>"
+dir=domain/$2
+url=https://psbdmp.ws/api/search/domain/$2
+mkdir ./output/$dir/
+test
+elif [[ $2 == '' ]]; then
+        echo "No input"
         exit 0
 fi
-
-
-
